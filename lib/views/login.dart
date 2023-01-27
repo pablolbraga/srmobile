cf662e80db5a6d6d1d65d2101607226fae185e7a
pablolbraga <pablolbraga@gmail.com>
2023-01-27 15:40:03 -0300

    Criação da tela de login

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -8,8 +10,180 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _ctrLogin = TextEditingController();
+  final TextEditingController _ctrSenha = TextEditingController();
+  bool _esconderSenha = true;
+  String _mensagemErro = "";
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return WillPopScope(
+      onWillPop: _voltarTela,
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(color: Color(0xffD9EFF3)),
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Image.asset(
+                      "images/LogoNovaTransparente.png",
+                      width: 300,
+                      height: 250,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: TextField(
+                      controller: _ctrLogin,
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Login",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.person,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: TextField(
+                      controller: _ctrSenha,
+                      obscureText: _esconderSenha,
+                      keyboardType: TextInputType.text,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Senha",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Colors.blue,
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            _mostrarSenha();
+                          },
+                          icon: Icon(
+                            _esconderSenha == true
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(32),
+                        ),
+                      ),
+                      child: const Text(
+                        "Acessar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onPressed: () {
+                        _validarCampos();
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Center(
+                      child: GestureDetector(
+                        child: const Text(
+                          "Esqueceu a senha? Clique aqui.",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _esqueceuSenha();
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Center(
+                      child: Text(
+                        _mensagemErro,
+                        style: const TextStyle(color: Colors.red, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  Future<bool> _voltarTela() async {
+    Navigator.pushNamed(context, "/");
+    return true;
+  }
+
+  void _mostrarSenha() {
+    setState(() {
+      _esconderSenha = !_esconderSenha;
+    });
+  }
+
+  void _validarCampos() {
+    String login = _ctrLogin.text;
+    String senha = _ctrSenha.text;
+
+    if (login.isNotEmpty) {
+      if (senha.isNotEmpty) {
+        setState(() {
+          _mensagemErro = "";
+          _acessarSistema();
+        });
+      } else {
+        setState(() {
+          _mensagemErro = "Senha não informada.";
+        });
+      }
+    } else {
+      setState(() {
+        _mensagemErro = "Login não informado.";
+      });
+    }
+  }
+
+  void _esqueceuSenha() {}
+
+  void _acessarSistema() {}
 }
