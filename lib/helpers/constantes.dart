df22679d2ac7338a541365479d8640b29a1e55a6
pablolbraga <pablolbraga@gmail.com>
2023-01-30 20:51:51 -0300

    Criação da rotina de registro da ficha de terapia

--- lib/helpers/constantes.dart
+++ lib/helpers/constantes.dart
@@ -7,6 +7,7 @@ const URL_ESQUECEU_SENHA = "${URL_BASE}esqueceusenha/";
 const URL_VALIDAR_LOGIN_SENHA = "${URL_BASE}validarloginsenha/";
 const URL_ALTERAR_SENHA = "${URL_BASE}alterarsenha/";
 const URL_LISTAR_AGENDA = "${URL_BASE}listaragendaporprofissional/";
+const URL_ADICIONAR_FICHA_TERAPIA = "${URL_BASE}addfichaterapia/";
 
 const List<String> LISTA_FICHATERAPIA_PARTICIPACAO = [
   'SELECIONE',
--- lib/views/assinaturaprofissional.dart
+++ lib/views/assinaturaprofissional.dart
@@ -1,8 +1,17 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:date_format/date_format.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:progress_dialog_null_safe/progress_dialog_null_safe.dart';
 import 'package:signature/signature.dart';
+import 'package:srmobile/helpers/constantes.dart';
+import 'package:srmobile/helpers/uteis.dart';
 import 'package:srmobile/helpers/variaveisglobais.dart';
 
 class AssinaturaProfissional extends StatefulWidget {
@@ -17,8 +26,21 @@ class _AssinaturaProfissionalState extends State<AssinaturaProfissional> {
       penStrokeWidth: 4,
       penColor: Colors.black,
       exportBackgroundColor: Colors.transparent);
+  final TextEditingController _ctrLatitude = TextEditingController();
+  final TextEditingController _ctrLongitude = TextEditingController();
+  String dataFinal = "";
+  String long = "";
+  String lat = "";
+  late ProgressDialog pr;
+  late Position position;
+
   @override
   Widget build(BuildContext context) {
+    pr = ProgressDialog(context, showLogs: true);
+    pr.style(message: "Enviando ficha...");
+    _getLocation();
+    dataFinal = formatDate(
+        DateTime.now(), [dd, '/', mm, '/', yyyy, ' ', HH, ':', nn, ':', ss]);
     return WillPopScope(
       onWillPop: _voltarTela,
       child: Scaffold(
@@ -57,6 +79,13 @@ class _AssinaturaProfissionalState extends State<AssinaturaProfissional> {
                           if (dataProfissional != null) {
                             VariaveisGlobais.assinaturaProfissional =
                                 base64Encode(dataProfissional);
+                            switch (VariaveisGlobais
+                                .dadosUsuario?.idespecialidade) {
+                              case 0:
+                                break;
+                              default:
+                                _enviarDadosTerapia();
+                            }
                           }
                         }
                       },
@@ -101,5 +130,47 @@ class _AssinaturaProfissionalState extends State<AssinaturaProfissional> {
   Future<bool> _voltarTela() async {
     Navigator.pushNamed(context, "assinatura_paciente");
     return true;
+  }
+
+  void _enviarDadosTerapia() async {
+    pr.show();
+    Dio dio = Dio();
+    Response response = await dio.post(URL_ADICIONAR_FICHA_TERAPIA,
+        data: VariaveisGlobais.dadosFichaTerapia?.toJson());
+    if (response.statusCode == 200) {
+      Future.delayed(const Duration(seconds: 10)).then((value) {
+        pr.hide().whenComplete(() {
+          Navigator.pushNamed(context, "agenda");
+        });
+      });
+    } else {
+      Uteis.mostrarAviso(context, "Erro",
+          "Erro ao enviar os dados. Erro: ${response.statusMessage}$response");
+    }
+  }
+
+  void _getLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    long = position.longitude.toString();
+    lat = position.latitude.toString();
+
+    setState(() {
+      _ctrLatitude.text = lat;
+      _ctrLongitude.text = long;
+    });
+
+    LocationSettings locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 100);
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      long = position.longitude.toString();
+      lat = position.latitude.toString();
+      setState(() {
+        _ctrLatitude.text = lat;
+        _ctrLongitude.text = long;
+      });
+    });
   }
 }
